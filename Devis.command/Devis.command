4898b71fb7cfe9d93a868cc1da03398602297b89
senterre <senterre@iMac-de-senterre-2.local>
2026-07-12 12:12:41 +0200

    Nettoie les fichiers suivis et fixe la nouvelle reference

--- Devis.command/Devis.command
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /Users/senterre/Desktop/MonProjetPython || exit 1
-python3 app_devis.py
-
--- app_devis.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-from main import HorizonChantierApp
-
-
-if __name__ == "__main__":
-    app = HorizonChantierApp()
-    app.mainloop()
